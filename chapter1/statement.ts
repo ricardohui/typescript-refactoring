ce5a61a969d333dd85aa628519f6b3ee578edfa9
RCD Hui <rcdhui@gmail.com>
2025-06-08 12:22:47 -0400

    extract method to amountFor

--- chapter1/statement.ts
+++ chapter1/statement.ts
@@ -9,11 +9,13 @@ type Invoice = {
   customer: string;
   performances: Performance[];
 };
+type Play = {
+  name: string;
+  type: string;
+};
+
 type Plays = {
-  [key: string]: {
-    name: string;
-    type: string;
-  };
+  [key: string]: Play;
 };
 
 function statement(invoice: Invoice, plays: Plays) {
@@ -27,24 +29,7 @@ function statement(invoice: Invoice, plays: Plays) {
   }).format;
   for(let perf of invoice.performances){
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch(play.type){
-      case "tragedy":
-        thisAmount = 40000;
-        if(perf.audience > 30){
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if(perf.audience > 20){
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(play, perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -56,5 +41,27 @@ function statement(invoice: Invoice, plays: Plays) {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function amountFor(play: Play, perf: Performance) {
+  let result = 0;
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20);
+      }
+      result += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
   return result;
 }
