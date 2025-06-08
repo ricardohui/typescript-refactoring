86dcf10764f5f815cd63643c04603b17614b98d1
RCD Hui <rcdhui@gmail.com>
2025-06-08 13:57:12 -0400

    extract function

--- chapter1/statement.ts
+++ chapter1/statement.ts
@@ -28,10 +28,23 @@ function statement(invoice: Invoice, plays: Plays) {
     totalAmount += amountFor(perf);
   }
   
-  let volumeCredits = 0;
-  for(let perf of invoice.performances){
-    volumeCredits += volumeCreditsFor(perf);
+  let volumeCredits = totalVolumeCredits(); 
+
+  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+  
+  
+  function totalVolumeCredits(){
+    let volumeCredits = 0;
+    for(let perf of invoice.performances){
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
+}
+
+
 
 function usd(aNumber: number) {
   return new Intl.NumberFormat("en-US", {
