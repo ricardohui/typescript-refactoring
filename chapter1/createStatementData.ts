8a3bce4cac24facd36a4e76b506fa9c820895d0e
RCD Hui <rcdhui@gmail.com>
2025-06-10 22:04:13 -0400

    Replace conditional with polymorphism

--- chapter1/createStatementData.ts
+++ chapter1/createStatementData.ts
@@ -1,18 +1,73 @@
 import { Invoice, Play, Plays, StatementData, Performance} from './types';
 
+class PerformanceCalculator {
+  play: Play;
+  performance: Performance;
+  constructor(aPerformance: Performance, aPlay: Play) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+   get amount() : number {
+      throw new Error("Subclass must override amount");
+    }
+
+    get volumeCredits() {
+      let result = 0;
+      result += Math.max(this.performance.audience - 30, 0);
+      return result;
+    }
+}
+
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5); 
+  }
+}
+
+function createPerformanceCalculator(aPerformance: Performance, aPlay: Play) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`Unknown type: ${aPlay.type}`);
+  }
+}
+
 export function createStatementData(invoice: Invoice, plays: Plays): StatementData {
     const statementData: StatementData = {
       customer: invoice.customer,
-      performances: invoice.performances.map(enrichPerformance)};
-      statementData.totalAmount = totalAmount(statementData);
+      performances: invoice.performances.map(enrichPerformance), }; statementData.totalAmount = totalAmount(statementData);
       statementData.totalVolumeCredits = totalVolumeCredits(statementData);
       return statementData;
   
  function enrichPerformance(aPerformance: Performance): Performance {
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance)); 
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(aPerformance);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
 
     function playFor(aPerformance: Performance): Play {
@@ -20,41 +75,7 @@ export function createStatementData(invoice: Invoice, plays: Plays): StatementDa
     }
 
   }
-  function playFor(perf: Performance): Play {
-    return plays[perf.playID as keyof typeof plays];
-  }
-
-    function amountFor(aPerformance: Performance) {
-      let result = 0;
-          switch (aPerformance.play?.type) {
-            case "tragedy":
-              result = 40000;
-              if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-              }
-              break;
-            case "comedy":
-              result = 30000;
-              if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-              }
-              result += 300 * aPerformance.audience;
-              break;
-            default:
-              throw new Error(`unknown type: ${aPerformance.play?.type}`);
-            }
-        return result;
-      }
-        function volumeCreditsFor(aperformance: Performance) {
-          let result = 0;
-          result += Math.max(aperformance.audience - 30, 0);
-          if ("comedy" ===aperformance.play?.type) {
-            result += Math.floor(aperformance.audience / 5);
-          }
-          return result;
-        }
-
-
+  
   function totalVolumeCredits(data: StatementData) {
     return data.performances.reduce((total, p) => total + p.volumeCredits!, 0);
   }
