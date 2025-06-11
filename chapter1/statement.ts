90c936ec00c721437bf89b977c98b0c33dae5185
RCD Hui <rcdhui@gmail.com>
2025-06-10 21:09:33 -0400

    add htmlStatatment and renderHTML

--- chapter1/statement.ts
+++ chapter1/statement.ts
@@ -2,10 +2,10 @@ import { Invoice, Plays, StatementData } from "./types";
 import { createStatementData } from "./createStatementData";
 
 export function statement(invoice: Invoice, plays: Plays) {
-  return renderplaintext(createStatementData(invoice, plays));
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderplaintext(data: StatementData) {
+function renderPlainText(data: StatementData) {
   let result = `Statement for ${data.customer}\n`;
 
   for(let perf of data.performances){
@@ -24,5 +24,25 @@ function usd(anumber: number) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(anumber/100);
+}
+
+
+export function htmlStatement(invoice: Invoice, plays: Plays) {
+ return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data: StatementData) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play?.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount!)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount!)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits} credits</em></p>\n`;
+  return result;
 }
 
