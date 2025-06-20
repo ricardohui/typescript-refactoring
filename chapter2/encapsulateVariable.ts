32f525f73cfa1e6d03d95adbf14d30e2841a634b
RCD Hui <rcdhui@gmail.com>
2025-06-20 00:11:12 -0400

    add getter and setter to control access to a widely used variable

--- chapter2/encapsulateVariable.ts
+++ chapter2/encapsulateVariable.ts
@@ -2,3 +2,10 @@ let defaultOwnerData = {
   firstName: "Martin",
   lastName: "Fowler"
 }
+
+export function defaultOwner() {
+  return defaultOwnerData;
+}
+export function setDefaultOwner(firstName: string, lastName: string) {
+  defaultOwnerData = { firstName, lastName };
+}
