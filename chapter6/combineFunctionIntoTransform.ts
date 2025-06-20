8baaa667e82db71d970f4f324c3cc4973c07e92f
RCD Hui <rcdhui@gmail.com>
2025-06-19 22:30:29 -0400

    add enrichReading

--- chapter6/combineFunctionIntoTransform.ts
+++ chapter6/combineFunctionIntoTransform.ts
@@ -1,6 +1,12 @@
-const aReading = acquireReading();
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
 const basicChargeAmount = calculateBaseCharge(aReading);
 
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+function enrichReading(original){
+  const result = Object.assign({}, original);
+  return result;
 }
