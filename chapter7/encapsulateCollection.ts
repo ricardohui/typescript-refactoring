dc63e414687150d3dac718c4aa5263859c026228
RCD Hui <rcdhui@gmail.com>
2025-06-23 21:19:13 -0400

    after encapsulate collection

--- chapter7/encapsulateCollection.ts
+++ chapter7/encapsulateCollection.ts
@@ -1,9 +1,22 @@
 class Person{
   _courses: string[];
+  
   get courses() {
-    return this._courses;
+    return this._courses ? this._courses.slice() : [];
   }
-  set courses(aList) {
-    this._courses = aList;
+
+  addCourse(course: string) {
+    if (!this._courses) {
+      this._courses = [];
+    }
+    this._courses.push(course);
+  }
+  removeCourse(course: string) {
+    if (this._courses) {
+      const index = this._courses.indexOf(course);
+      if (index !== -1) {
+        this._courses.splice(index, 1);
+      }
+    }
   }
 }
