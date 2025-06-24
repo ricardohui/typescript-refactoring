a83a040a0a26eff67f32e8e0d5787f6c03201910
RCD Hui <rcdhui@gmail.com>
2025-06-24 19:45:26 -0400

    after inline class

---
+++ chapter7/extractClass.ts
@@ -0,0 +1,33 @@
+class Person{
+  _name: string;
+  _officeAreaCode: string;
+  _officeNumber: string;
+  constructor(name, officeAreaCode, officeNumber) {
+    this._name = name;
+    this._officeAreaCode = officeAreaCode;
+    this._officeNumber = officeNumber;
+  }
+  get name(){
+    return this._name;
+  }
+  set name(arg){
+    this._name = arg;
+  }
+
+  get telephoneNumber(){
+    return `${this.officeAreaCode}-${this.officeNumber}`;
+  }
+
+  get officeAreaCode(){
+    return this._officeAreaCode;
+  }
+  set officeAreaCode(arg){
+    this._officeAreaCode = arg;
+  }
+  get officeNumber(){
+    return this._officeNumber;
+  }
+  set officeNumber(arg){
+    this._officeNumber = arg;
+  }
+}
