cd3485aaadb8740a895e0c23e006fe78f9228ea5
RCD Hui <rcdhui@gmail.com>
2025-06-23 21:55:09 -0400

    after replace primitive with object

--- chapter7/replacePrimitiveWithObject.ts
+++ chapter7/replacePrimitiveWithObject.ts
@@ -1,2 +1,51 @@
-orders.filter(order => "high" === order.priority || "rush" === order.priority);
+([] as Order[]).filter(order => {
+  return order.priority.higherThan(new Priority("normal"));
+})
 
+class Priority {
+  constructor(public value: any) {
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value))
+      this.value = value;
+    else
+      throw new Error(`Invalid priority value: ${value}`);
+    }
+
+    toString(){
+      return this.value;
+    }
+
+    get _index(){ return Priority.legalValues().findIndex(v => v === this.value); }
+
+    static legalValues() {
+      return ["low", "normal", "high", "rush"];
+    }
+    equals(other: Priority) {
+      if (other instanceof Priority) {
+        return this._index === other._index;
+      }
+      return false;
+    }
+    higherThan(other: Priority) {
+      if (other instanceof Priority) {
+        return this._index > other._index;
+      }
+      return false;
+    }
+    lowerThan(other: Priority) {
+      if (other instanceof Priority) {
+        return this._index < other._index;
+      }
+      return false;
+    }
+}
+class Order{
+  _priority: Priority;
+  get priority() {
+    return this._priority;
+  }
+
+  set priority(value) {
+    this._priority = value;
+  }
+}
