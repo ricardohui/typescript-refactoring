0a865912b43746d29d673300b611a14b3e45dae1
RCD Hui <rcdhui@gmail.com>
2025-06-23 20:57:43 -0400

    after Encapsulate Record

--- chapter7/encapsulateRecord.ts
+++ chapter7/encapsulateRecord.ts
@@ -2,3 +2,27 @@ const organization = {
   name: "Tech Corp",
   country: "GB"
 };
+
+
+class Organziation{
+  _name;
+  _country;
+
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get country() {
+    return this._country;
+  } 
+  set name(arg) {
+    this._name = arg;
+  }
+  set country(arg) {
+    this._country = arg;
+  }
+}
