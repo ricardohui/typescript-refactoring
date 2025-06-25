cf7c3ce95e22ec21a778b6cf53b5c2ad58f5b496
RCD Hui <rcdhui@gmail.com>
2025-06-24 22:39:56 -0400

    after substitute algorithm

--- chapter7/substituteAlgorithm.ts
+++ chapter7/substituteAlgorithm.ts
@@ -1,13 +1,4 @@
 function foundPerson(people){
-  for (let i = 0; i < people.length; i++) {
-    if(people[i].name === "Don"){
-      return "Don"
-    }
-    if(people[i].name === "John"){
-      return "John"
-    }
-    if(people[i].name === "Kent"){
-      return "Kent"
-    } 
-  }
+  const candidates = ["Don", "John", "James"];
+  return people.find((p) => candidates.includes(p.name) || "");
 }
