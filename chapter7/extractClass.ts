2afedd3baead83e9bdb50d2a01b21bf342dfff41
RCD Hui <rcdhui@gmail.com>
2025-06-24 20:51:13 -0400

    after Extract Class

--- chapter7/extractClass.ts
+++ chapter7/extractClass.ts
@@ -1,11 +1,9 @@
 class Person{
   _name: string;
-  _officeAreaCode: string;
-  _officeNumber: string;
+  _telephoneNumber: telephoneNumber;
   constructor(name, officeAreaCode, officeNumber) {
     this._name = name;
-    this._officeAreaCode = officeAreaCode;
-    this._officeNumber = officeNumber;
+    this._telephoneNumber = new telephoneNumber();
   }
   get name(){
     return this._name;
@@ -15,19 +13,42 @@ class Person{
   }
 
   get telephoneNumber(){
-    return `${this.officeAreaCode}-${this.officeNumber}`;
+    return `${this._telephoneNumber.toString()}`;
   }
 
   get officeAreaCode(){
-    return this._officeAreaCode;
+    return this._telephoneNumber.areaCode;
   }
   set officeAreaCode(arg){
-    this._officeAreaCode = arg;
+    this._telephoneNumber.areaCode = arg;
   }
   get officeNumber(){
-    return this._officeNumber;
+    return this._telephoneNumber.number;
   }
   set officeNumber(arg){
-    this._officeNumber = arg;
+    this._telephoneNumber.number = arg;
   }
+}
+
+class telephoneNumber{
+
+  _areaCode: string;
+  _number: string;
+
+  get areaCode(){
+    return this.areaCode;
+  }
+  set areaCode(arg){
+    this.areaCode = arg;
+  }
+  get number(){
+    return this._number;
+  }
+  set number(arg){
+    this._number = arg;
+  }
+
+  toString(){
+    return `${this._areaCode}-${this._number}`;
+  }  
 }
