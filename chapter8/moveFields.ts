743551ee8410663c0e44cab2b7e1af23d63dc865
RCD Hui <rcdhui@gmail.com>
2025-06-25 22:12:14 -0400

    after movefield

--- chapter8/moveFields.ts
+++ chapter8/moveFields.ts
@@ -1,26 +1,46 @@
 class Customer{
+  _discountRate;
+  _name;
+  _contract;
+
   constructor(name, discountRate) {
     this._name = name;
+    this._setDiscountRate(discountRate);
+    const dateToday = () => new Date();
+    this._contract = new CustomerContract(dateToday(), discountRate); 
+  }
+
+  get discountRate() {
+    return this._contract._discountRate;
+  }
+
+  _setDiscountRate(value) {
+    this._contract._discountRate = value;
+  }
+
+  becomePreferred() {
+    this._setDiscountRate(this.discountRate + 0.03);
+  }
+
+  applyDiscount(amount) {
+    return amount.substract(amount.multiply(this.discountRate));
+  }
+}
+
+class CustomerContract {
+  _startDate;
+  _discountRate;
+  constructor(startDate, discountRate) {
+    this._startDate = startDate;
     this._discountRate = discountRate;
-    this._contract = new CustomerContract(dateToday());
   }
 
   get discountRate() {
     return this._discountRate;
   }
 
-  becomePreferred() {
-    this._discountRate += 0.03;
-  }
-
-  applyDiscount(amount) {
-    return amount.substract(amount.multiply(this._discountRate));
-  }
-}
-
-class CustomerContract {
-  constructor(startDate) {
-    this._startDate = startDate;
+  set discountRate(value) {
+    this._discountRate = value;
   }
 
   get startDate() {
