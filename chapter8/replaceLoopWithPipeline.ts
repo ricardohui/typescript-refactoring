63b723a740ab503103ada8f3c6c45d619a12b5ff
RCD Hui <rcdhui@gmail.com>
2025-06-26 23:01:39 -0400

    inline result

--- chapter8/replaceLoopWithPipeline.ts
+++ chapter8/replaceLoopWithPipeline.ts
@@ -1,12 +1,10 @@
 function acquireDaa(input: string){
   const lines = input.split('\n');
-  type result = {city: string, phone: string}[];
-  const result = lines
+  return lines
     .slice(1) // Skip the first line
     .filter(line => line.trim() !== "") // Filter out empty lines
     .map(line => line.split('.'))
     .filter(record => record[1].trim() === "India") // Filter records where country is India
     .map(record => ({ city: record[0].trim(), phone: record[2].trim() })) // Map to desired structure
     ;
-  return result;
 }
