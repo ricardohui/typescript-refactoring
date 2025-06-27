d4959b1f6745ea0c0098758a9a568cc82da382d5
RCD Hui <rcdhui@gmail.com>
2025-06-26 21:34:08 -0400

    slide statements

--- chapter8/slideStatements.ts
+++ chapter8/slideStatements.ts
@@ -1,9 +1,8 @@
 let result;
 if (availableResources.length === 0) {
   result = createResource();
-  allocateResources.push(result);
 } else {
   result = availableResources.pop();
-  allocateResources.push(result);
 }
+allocateResources.push(result);
 return result;
