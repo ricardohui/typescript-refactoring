dfabb6656f49f9fbb683b6eb42e25b8ae711912c
RCD Hui <rcdhui@gmail.com>
2025-06-26 22:51:21 -0400

    replace split

--- chapter8/replaceLoopWithPipeline.ts
+++ chapter8/replaceLoopWithPipeline.ts
@@ -3,9 +3,11 @@ function acquireDaa(input: string){
   const result: {city: string, phone: string}[] = [];
   const loopItems = lines
     .slice(1) // Skip the first line
-    .filter(line => line.trim() !== ""); // Filter out empty lines
+    .filter(line => line.trim() !== "") // Filter out empty lines
+    .map(line => line.split('.'))
+    ;
   for (const line of loopItems) {
-    const record = line.split(',');
+    const record = line;
     if (record[1].trim() === "India") {
       result.push({city: record[0].trim(), phone: record[2].trim()});
     }
