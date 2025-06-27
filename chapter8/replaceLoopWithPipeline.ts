a851c0a6c6daf6089c3f86fb463538d359af479a
RCD Hui <rcdhui@gmail.com>
2025-06-26 22:47:38 -0400

    replace with a filter operation

--- chapter8/replaceLoopWithPipeline.ts
+++ chapter8/replaceLoopWithPipeline.ts
@@ -3,10 +3,8 @@ function acquireDaa(input: string){
   const result: {city: string, phone: string}[] = [];
   const loopItems = lines
     .slice(1) // Skip the first line
+    .filter(line => line.trim() !== ""); // Filter out empty lines
   for (const line of loopItems) {
-    if (line.trim()==='') {
-      continue;
-    }
     const record = line.split(',');
     if (record[1].trim() === "India") {
       result.push({city: record[0].trim(), phone: record[2].trim()});
