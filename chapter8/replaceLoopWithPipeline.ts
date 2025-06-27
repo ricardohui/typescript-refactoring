b6071d46252e168f1e7ccfd92f583460d1538676
RCD Hui <rcdhui@gmail.com>
2025-06-26 22:42:29 -0400

    to start with replace loop with pipeline

---
+++ chapter8/replaceLoopWithPipeline.ts
@@ -0,0 +1,19 @@
+function acquireDaa(input: string){
+  const lines = input.split('\n');
+  let firstLine = true;
+  const result: {city: string, phone: string}[] = [];
+  for (const line of lines) {
+    if(firstLine){
+      firstLine = false;
+      continue;
+    }
+    if (line.trim()==='') {
+      continue;
+    }
+    const record = line.split(',');
+    if (record[1].trim() === "India") {
+      result.push({city: record[0].trim(), phone: record[2].trim()});
+    }
+  }
+  return result;
+}
