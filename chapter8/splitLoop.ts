776aa57fc06736aeb51425a934420c06795f9d43
RCD Hui <rcdhui@gmail.com>
2025-06-26 22:21:31 -0400

    slide statement

--- chapter8/splitLoop.ts
+++ chapter8/splitLoop.ts
@@ -5,12 +5,12 @@ function toBeSplitLoop() {
     { name: "James", age: 35, salary: 70000 },
     { name: "Alice", age: 28, salary: 55000 },
   ];
-  let youngest = people[0] ? people[0].age : 999999999999999;
   let totalSalary = 0;
   for (const person of people) {
     totalSalary += person.salary;
   }
   
+  let youngest = people[0] ? people[0].age : 999999999999999;
   for (const person of people) {
     if (person.age < youngest) {
       youngest = person.age;
