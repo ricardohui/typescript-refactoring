71d0bfbc97c1c0684dd6186ba0585ba04bf06120
RCD Hui <rcdhui@gmail.com>
2025-07-01 16:08:39 -0400

    make it  all understandable

--- chapter10/consolidateConditionalExpression.ts
+++ chapter10/consolidateConditionalExpression.ts
@@ -1,5 +1,9 @@
 function disabilityAmount(anEmployee){
-  if(anEmployee.seniority < 2 || anEmployee.monthDisabled > 12 || anEmployee.isPartTime) return 0;
+  if(isNotEligibleForDisability(anEmployee)) return 0;
   // compute the disability amount
   return anEmployee.monthsDisabled * 100;
+}
+
+function isNotEligibleForDisability(anEmployee: any) {
+  return anEmployee.seniority < 2 || anEmployee.monthDisabled > 12 || anEmployee.isPartTime;
 }
