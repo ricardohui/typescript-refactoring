d3a4d83a86025989c41a6645a707befb571eb2d3
RCD Hui <rcdhui@gmail.com>
2025-07-01 14:51:54 -0400

    extract function for condition

--- chapter10/decomposeConditional.ts
+++ chapter10/decomposeConditional.ts
@@ -1,5 +1,9 @@
-if(!aData.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+if(isSummer()) {
   charge = quantity * plan.summerRate;
 } else {
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
+}
+
+function isSummer() {
+  return !aData.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
 }
