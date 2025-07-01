de1a8497621a829004ac7b5b87d31ed8cc0d02af
RCD Hui <rcdhui@gmail.com>
2025-07-01 16:02:17 -0400

    combine one conditions using a logical operator

--- chapter10/consolidateConditionalExpression.ts
+++ chapter10/consolidateConditionalExpression.ts
@@ -1,6 +1,5 @@
 function disabilityAmount(anEmployee){
-  if(anEmployee.seniority < 2) return 0;
-  if(anEmployee.monthsDisabled > 12) return 0;
+  if(anEmployee.seniority < 2 || anEmployee.monthDisabled > 12) return 0;
   if(anEmployee.isPartTime) return 0;
   // compute the disability amount
   return anEmployee.monthsDisabled * 100;
