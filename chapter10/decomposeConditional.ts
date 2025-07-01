06cf2bf6ea723a5d30546f1b01cdb50baa953276
RCD Hui <rcdhui@gmail.com>
2025-07-01 14:48:55 -0400

    before decompose conditional

---
+++ chapter10/decomposeConditional.ts
@@ -0,0 +1,5 @@
+if(!aData.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+  charge = quantity * plan.summerRate;
+} else {
+  charge = quantity * plan.regularRate + plan.regularServiceCharge;
+}
