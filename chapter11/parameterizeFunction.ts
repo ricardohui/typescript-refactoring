744f78d3426f86328aa2187bc4ce7f2c4f967db7
RCD Hui <rcdhui@gmail.com>
2025-07-03 21:06:08 -0400

    refactor topBand

--- chapter11/parameterizeFunction.ts
+++ chapter11/parameterizeFunction.ts
@@ -1,15 +1,11 @@
 function baseCharge(usage){
   if (usage < 0) return usd(0);
-  const amount = withBand(usage, 0, 100) * 0.03 + withBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
+  const amount = withBand(usage, 0, 100) * 0.03 + withBand(usage, 100, 200) * 0.05 + withBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
 function withBand(usage, bottom, top){
   return usage > bottom ? Math.min(usage, top) - bottom : 0;
-}
-
-function topBand(usage){
-  return usage > 200 ? usage - 200 : 0;
 }
 
 function usd(amount: number): string {
