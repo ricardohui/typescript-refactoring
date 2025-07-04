0bd9b3e3fcbd1c8f1b4c8e0dd535e73c8df3d866
RCD Hui <rcdhui@gmail.com>
2025-07-03 20:59:25 -0400

    add withinBand function

--- chapter11/parameterizeFunction.ts
+++ chapter11/parameterizeFunction.ts
@@ -1,17 +1,25 @@
 function baseCharge(usage){
   if (usage < 0) return usd(0);
-  const amount = bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+  const amount = bottomBand(usage) * 0.03 + withBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
   return usd(amount);
+}
+
+function withBand(usage, bottom, top){
+  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 }
 
 function bottomBand(usage){
   return Math.min(usage, 100);
 }
 
-function middleBand(usage){
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+function withBand(usage, bottom, top){
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function topBand(usage){
   return usage > 200 ? usage - 200 : 0;
+}
+
+function usd(amount: number): string {
+  return `$${amount.toFixed(2)}`;
 }
