c0da8da61e3c2e0d2b0fc784743a53e565c32f8e
RCD Hui <rcdhui@gmail.com>
2025-07-05 01:28:48 -0400

    before refactoring

---
+++ chapter11/replaceQueryWithParameter.ts
@@ -0,0 +1,45 @@
+class Thermostat {
+  selectedTemperature: number;
+  currentTemperature: number;
+
+  constructor(selectedTemperature: number, currentTemperature: number) {
+    this.selectedTemperature = selectedTemperature;
+    this.currentTemperature = currentTemperature;
+  }
+}
+
+class HeatingPlan {
+  _min: number;
+  _max: number;
+
+  constructor(min: number, max: number) {
+    this._min = min;
+    this._max = max;
+  }
+
+  get targetTemperature() {
+    if (thermostat.selectedTemperature > this._max) return this._max;
+    else if (thermostat.selectedTemperature < this._min) return this._min;
+    else return thermostat.selectedTemperature;
+  }
+}
+
+// Mock functions for heating/cooling/off
+function setToHeat() {
+  console.log("Heating is ON");
+}
+function setToCool() {
+  console.log("Cooling is ON");
+}
+function setToOff() {
+  console.log("System is OFF");
+}
+
+// Example objects
+const thermostat = new Thermostat(22, 20);
+const thePlan = new HeatingPlan(18, 25);
+
+// Main logic
+if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+else setToOff();
