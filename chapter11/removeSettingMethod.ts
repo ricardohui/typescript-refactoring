08ecfd7308da30d402376df99b3edc9aba31051a
RCD Hui <rcdhui@gmail.com>
2025-07-05 01:53:46 -0400

    inline function

--- chapter11/removeSettingMethod.ts
+++ chapter11/removeSettingMethod.ts
@@ -1,11 +1,12 @@
 class Person{
+constructor(id) {
+  this._id = id;
+}
   get name(){ return this._name;}
   set name(arg){ this._name = arg;}
   get id() { return this._id; }
-  set id(arg) { this._id = arg; } 
 }
 
 
-const martin = new Person();
+const martin = new Person("1234");
 martin.name = "Martin";
-martin.id = "12345";
