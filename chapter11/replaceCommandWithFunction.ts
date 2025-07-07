ff8de2cd92c0a745dcca01a1e30846b7b01387f3
RCD Hui <rcdhui@gmail.com>
2025-07-06 23:13:11 -0400

    inlien function

--- chapter11/replaceCommandWithFunction.ts
+++ chapter11/replaceCommandWithFunction.ts
@@ -4,14 +4,13 @@ class ChargeCalculator{
   }
 
   charge(customer, usage, provider){
-    const baseCharge = customer.baseRate * usage;
-    return baseCharge + provider.connectionCharge;
-  }
+   }
 
 }
 
 const monthCharge = charge(customer, usage, provider);
 
-function charge(customer, usage, provider){
-  return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
