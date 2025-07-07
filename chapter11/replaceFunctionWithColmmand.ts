3ed1618053f2a4d21f4ba9d9c5e9f9ce68aadff3
RCD Hui <rcdhui@gmail.com>
2025-07-06 22:40:22 -0400

    move local variable to field

--- chapter11/replaceFunctionWithColmmand.ts
+++ chapter11/replaceFunctionWithColmmand.ts
@@ -6,27 +6,31 @@ class Scorer {
   _candidate
   _medicalExam;
   _scoringGuide;
+  _result;
+  _healthLevel;
+  _highMedicalRiskFlag;
+
   constructor(candidate, medicalExam, scoringGuide){
     this._candidate = candidate;
     this._medicalExam = medicalExam;
     this._scoringGuide = scoringGuide;
   }
   execute(){
-  let result = 0;
-  let healthLevel = 0;
-  let highMeidcalRiskFlag = false;
+  this._result = 0;
+  this._healthLevel = 0;
+  this._highMedicalRiskFlag = false;
 
   if(this._medicalExam.isSmoker){
-    healthLevel += 10;
-    highMeidcalRiskFlag = true;
+    this._healthLevel += 10;
+    this._highMedicalRiskFlag = true;
   }
   let certificationGrade = "regular";
   if (this._scoringGuide.stateWithLowCertificate(this._candidate.orgiinState){
     certificationGrade = "low";
-    result -= 5;
+    this._result -= 5;
   }
   // lot more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
+  this._result -= Math.max(this._healthLevel - 5, 0);
+  return this._result;
   }
 }
