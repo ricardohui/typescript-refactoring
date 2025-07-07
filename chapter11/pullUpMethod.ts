ae6033518c5d566a97bff04ed00586dacb5841b4
RCD Hui <rcdhui@gmail.com>
2025-07-07 00:19:53 -0400

    change function declaration

--- chapter11/pullUpMethod.ts
+++ chapter11/pullUpMethod.ts
@@ -5,7 +5,18 @@ class Employee extends Party{
 }
 
 class Department extends Party{
-  get totalAnnualCost(){
+  get annualCost(){
     return this.monthlyCost * 12;
+  }
+}
+
+
+class Party{
+  get annualCost(){
+    return this.monthlyCost * 12;
+  }
+
+  get monthlyCost(){ // let subcalas implement this
+    throw new SubclassResponsibilityError();
   }
 }
