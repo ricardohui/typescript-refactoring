39507b335aad5978da5edfc7cfcefc2432754066
RCD Hui <rcdhui@gmail.com>
2025-07-09 20:59:07 -0400

    before refactoring

---
+++ chapter12/removeSubclass.ts
@@ -0,0 +1,35 @@
+class Person{
+  _name;
+  constructor(name){
+    this._name = name;
+  }
+
+  get name(){return this._name;}
+  get genderCode(){return "X";}
+}
+
+class Male extends Person{
+  get genderCode(){return "M"}
+}
+
+class Female extends Person{
+  get genderCode(){ return "F"}
+}
+
+//client
+function loadFromInput(data){
+  const result = [];
+  data.forEach(aRecord =>{
+    let p;
+    switch(aRecord.gender){
+      case 'M': p = new Male(aRecord.name); break;
+      case 'F': p = new Female(aRecord.name); break;
+      default: p = new Person(aRecord.name);
+    }
+    result.push(p);
+  })
+  return result;
+}
+
+
+const numberOfMales = people.filter(p => p instanceof Male).length;
