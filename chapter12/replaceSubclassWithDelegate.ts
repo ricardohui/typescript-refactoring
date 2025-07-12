117bd9b70cf7593cb6ca8104399838ed51820f2a
RCD Hui <rcdhui@gmail.com>
2025-07-11 21:11:04 -0400

    get rid of EurpeanSwallow

--- chapter12/replaceSubclassWithDelegate.ts
+++ chapter12/replaceSubclassWithDelegate.ts
@@ -1,9 +1,19 @@
 class Bird {
   _name: string;
   _plumage;
+  _speciesDelegate;
   constructor(data){
     this._name = data.name;
     this._plumage = data.plumage;
+    this._speciesDelegate = this.selectSpeciesDelegate(data);
+  }
+
+  selectSpeciesDelegate(data){
+    switch(data.type){
+      case 'EuropeanSwallow':
+        return new EuropeanSwallowDelegate();
+      default: return null;
+    }
   }
 
   get name(){ return this._name;}
@@ -12,13 +22,10 @@ class Bird {
     return this._plumage || "average"
   }
   
-  get airSpeedVelocity(): number | null{return null;}
+  get airSpeedVelocity(): number | null{
+    return this._speciesDelegate ? this._speciesDelegate.airSpeedVelocity :null;}
 }
 
-class EuropeanSwallow extends Bird{
-  get airSpeedVelocity(){ return 35;}
-
-}
 
 class AfricanSwallow extends Bird{
   _numberOfCoconuts;
@@ -52,11 +59,12 @@ class NorwegianBlueParrot extends Bird{
 }
 
 
+class EuropeanSwallowDelegate{
+  get airSpeedVelocity(){ return 35;}
+}
 
 function createBird(data){
   switch (data.type){
-    case 'EuropeanSwallow':
-      return new EuropeanSwallow(data) 
     case 'AfricanSwallow':
       return new AfricanSwallow(data)
     case 'NorwegianBlueParrot':
