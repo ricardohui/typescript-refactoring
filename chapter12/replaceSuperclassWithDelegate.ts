0b464384e6c6921a4e7f3db287c359a8bee45dd9
RCD Hui <rcdhui@gmail.com>
2025-07-13 00:01:39 -0400

    ading catalogitem as delegate

--- chapter12/replaceSuperclassWithDelegate.ts
+++ chapter12/replaceSuperclassWithDelegate.ts
@@ -19,8 +19,10 @@ class CatalogItem{
 
 class Scroll extends CatalogItem {
   _lastCleaned
+  _catalogItem
   constructor(id, title, tags, dataLastCleaned) {
     super(id, title, tags);
+    this._catalogItem = new CatalogItem(id, title, tags);
     this._lastCleaned = dataLastCleaned;
   }
 
@@ -32,4 +34,11 @@ class Scroll extends CatalogItem {
   daysSinceLastCleaning(targetDate){
     return this._lastCleaned.until(targetDate, ChronoUnit.DAYS)
   }
+
+  get id(){
+    return this._id;
+  }
+
+  get title(){return this._catalogItem.title;}
+  hasTag(aString){return this._catalogItem.hasTag(aString)}
 }
